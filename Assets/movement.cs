9594cc00281254a1906baa1945346950a0441042
MichKow525 <s25144@pjwstk.edu.pl>
2023-03-06 10:48:10 +0100

    movement_1

---
+++ Assets/movement.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Runtime.CompilerServices;
+using System.Threading;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class movement : MonoBehaviour
+{
+    public Rigidbody2D rb;
+    public int upForce = 200;
+    public float speed = 10;
+    public float runspeed = 15;
+    public bool isGrounded = false;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {  
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            rb.velocity = new Vector2(Input.GetAxis("Horizontal") * runspeed * Time.deltaTime, rb.velocity.y);
+        }
+        else
+        {
+            rb.velocity = new Vector2(Input.GetAxis("Horizontal") * speed * Time.deltaTime, rb.velocity.y);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space)&& isGrounded)
+        {
+
+            rb.AddForce(Vector2.up * upForce);
+            isGrounded = false;
+        } 
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        isGrounded = true;
+    }
+}
