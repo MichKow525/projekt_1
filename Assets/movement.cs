a667834c893a84a7d9a53ff9352b7146937000d1
MichKow525 <s25144@pjwstk.edu.pl>
2023-03-26 15:56:35 +0200

    animacje2

--- Assets/movement.cs
+++ Assets/movement.cs
@@ -6,18 +6,22 @@ using System.Threading;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using Unity.VisualScripting;
 
 public class movement : MonoBehaviour
 {
     public Rigidbody2D rb;
+    public int burnForce = 400;
     public int upForce = 200;
     public float speed = 10;
     public float runspeed = 15;
+    public bool isDamaged = false;
     public bool isGrounded = false;
     public bool doubleJump = false;
     public Animator kontroler;
     public Transform _originalParent;
-
+    public Vector3 lastMove;
+    public int lives = 2;
 
     // Start is called before the first frame update
     void Start()
@@ -29,41 +33,7 @@ public class movement : MonoBehaviour
     void Update()
     {   DoubleJump();
         Skok();
-
-        float move = Input.GetAxis("Horizontal");
-        if (move == 0)
-        {
-            kontroler.SetBool("isRun", false);
-        }
-        else
-        {
-            if (move > 0)
-            {
-                transform.localScale = new Vector3(5, 5, 1);
-            }
-            else if (move < 0)
-            {
-                transform.localScale = new Vector3(-5, 5, 1);
-            }
-
-            if (Input.GetKey(KeyCode.LeftShift))
-            {
-                kontroler.SetBool("isRun", true);
-                rb.velocity = new Vector2(move * runspeed * Time.deltaTime, rb.velocity.y);
-            }
-            else
-            {
-                kontroler.SetBool("isRun", true);
-                rb.velocity = new Vector2(move * speed * Time.deltaTime, rb.velocity.y);
-            }
-        }
-
-        if (Input.GetKey(KeyCode.Space))
-        {
-            kontroler.SetBool("isJump", true);
-           
-        }
-       
+        animacje();
     }
 
     void FixedUpdate()
@@ -76,7 +46,10 @@ public class movement : MonoBehaviour
         isGrounded = true;
         doubleJump = false;
     }
-
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        isGrounded = false;
+    }
     private void Skok()
  {
      if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
@@ -109,15 +82,27 @@ public class movement : MonoBehaviour
         {
             rb.velocity = new Vector2(Input.GetAxis("Horizontal") * speed * Time.deltaTime, rb.velocity.y);
         }
+        
   }
   
  private void OnTriggerEnter2D (Collider2D collision)
- { 
-     if (collision.tag == "dziura")
-     {
-            SceneManager.LoadScene(SceneManager.GetActiveScene().name);;
+    {
+        if (collision.tag == "lawa")
+        {
+            kontroler.SetBool("isDamaged", true);
+            isDamaged = true;
+            lives--;
+            rb.AddForce(Vector2.up * burnForce);
+            Invoke("nodamage", 1);
+
         }
- }
+
+        if (collision.tag == "dziura")
+     {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+       
+    }
     public void SetParent(Transform newParent)
     {
         _originalParent = transform.parent;
@@ -126,4 +111,64 @@ public class movement : MonoBehaviour
     public void ResetParent()
     { transform.parent = _originalParent; }
 
+    public void animacje()
+    {
+
+        float move = Input.GetAxis("Horizontal");
+        if (move == 0)
+        {
+            kontroler.SetBool("isRun", false);
+        }
+        else
+        {
+            if (move > 0)
+            {
+                transform.localScale = new Vector3(5, 5, 1);
+            }
+            else if (move < 0)
+            {
+                transform.localScale = new Vector3(-5, 5, 1);
+            }
+
+            if (Input.GetKey(KeyCode.LeftShift))
+            {
+                kontroler.SetBool("isRun", true);
+                rb.velocity = new Vector2(move * runspeed * Time.deltaTime, rb.velocity.y);
+            }
+            else
+            {
+                kontroler.SetBool("isRun", true);
+                rb.velocity = new Vector2(move * speed * Time.deltaTime, rb.velocity.y);
+            }
+        }
+
+        if (isGrounded == true)
+        {
+            kontroler.SetBool("isJump", false);
+        }
+        else
+        {
+            kontroler.SetBool("isJump", true);
+        }
+        if (Input.GetKey(KeyCode.Space))
+        {
+            kontroler.SetBool("isJump", true);
+        }
+        if (lives == 0)
+        {
+            kontroler.SetBool("isDead", true);
+            Invoke("death", 2);
+        }
+        kontroler.SetFloat("yVelocity", rb.velocity.y);
+
+    }
+    public void nodamage()
+    {
+        isDamaged = false;
+        kontroler.SetBool("isDamaged", false);
+    }
+    public void death()
+    {   
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
 }
